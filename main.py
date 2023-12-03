b6d692ab3638c82c0b30f0f06511b3c86b9e25c7
Amit Serper <amit@whitelodge.localdomain>
2023-12-03 10:37:46 -0500

    Switched to real-time alerts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from mastodon import Mastodon
-import json, requests
+import json
+from sseclient import SSEClient
 from datetime import datetime
 
 today_date = datetime.now().date()
@@ -11,24 +12,47 @@ formatted_date = today_date.strftime("%Y-%m-%d")
 
 Mastodon.create_app(
     'rocketalert',
-    api_base_url = 'https://mastodon.social',
-    to_file = 'pytooter_clientcred.secret'
+    api_base_url='https://mastodon.social',
+    to_file='pytooter_clientcred.secret'
 )
 
-
-# Then, log in. This can be done every time your application starts (e.g. when writing a
+# Then, log in. This can be done every time your application starts (e.g., when writing a
 # simple bot), or you can use the persisted information:
-mastodon = Mastodon(client_id = 'pytooter_clientcred.secret',)
+mastodon = Mastodon(client_id='pytooter_clientcred.secret',)
 mastodon.log_in(
     'aserper+rocketalert@gmail.com',
     '!)5oMS@MFJp$vOw4+NAK',
-    to_file = 'pytooter_usercred.secret'
+    to_file='pytooter_usercred.secret'
 )
-json_data = requests.get("https://ra-agg.kipodopik.com/api/v1/alerts/daily?from={0}&to={0}".format(formatted_date)).content
-data = json.loads(json_data)
-payload = data["payload"]
 
-# Note that this won't work when using 2FA - you'll have to use OAuth, in that case.
-# To post, create an actual API instance:
-mastodon = Mastodon(access_token = 'pytooter_usercred.secret')
-mastodon.toot('Number of alerts in Israel today: {}. Learn more at https://rocketalert.live #IsraelUnderAttack #Israel #rockets'.format(str(payload[0]['alerts'])))
+# Define a function to handle SSE events and post to Mastodon
+def handle_sse_events(mastodon_instance):
+    sse_url = "https://ra-agg.kipodopik.com/api/v1/alerts/real-time"  # Replace with your SSE stream URL
+    client = SSEClient(sse_url)  # Create the SSEClient object
+    try:
+        for event in client:
+            if event.event == 'message':
+                try:
+                    if event.data:
+                        data = json.loads(event.data)
+                        area_name_en = data.get('areaNameEn', '')
+                        timestamp = data.get('timeStamp', '')
+
+                        # Create the message text
+                        message_text = f"🚨🚨🚨 Rocket alert in Israel 🚨🚨🚨\n Area Name: {area_name_en}\nTimestamp: " \
+                                       f"{timestamp}\n Learn more at https://rocketalert.live"
+
+                        # Post the message to Mastodon
+                        mastodon_instance.toot(message_text)
+                except Exception as e:
+                    print(f"Error processing SSE event: {e}")
+    finally:
+        client.close()  # Close the SSEClient when done
+
+# Main script
+if __name__ == "__main__":
+    # Use the persisted Mastodon information to log in
+    mastodon_user = Mastodon(access_token='pytooter_usercred.secret')
+
+    # Start listening to SSE events and posting to Mastodon
+    handle_sse_events(mastodon_user)
