4f89c2b9b4a1a242ad65336166a27c364e972863
Amit Serper <aserper@gmail.com>
2024-03-05 21:20:09 -0500

    fix

--- rocket_alert_api.py
+++ rocket_alert_api.py
@@ -4,10 +4,10 @@ import requests
 class RocketAlertAPI:
     def __init__(self):
         self.baseURL = "https://agg.rocketalert.live/api/v1/alerts"
-        self.customHeaderKey = os.environ['CUSTOM_HEADER_KEY']
+        self.customHeaderValue = os.environ['CUSTOM_HEADER_KEY']
         self.customHeaderKey = "x-ra-agg-secret"
         self.headers = {
-            self.customHeaderKey,
+            self.customHeaderKey: self.customHeaderValue,
             # Custom header to please CF
             "user-agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0"
         }
